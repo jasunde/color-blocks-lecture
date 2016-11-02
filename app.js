ab8a6ebf663cd23168d79de9a1febca4088e47d2
jasunde <jlaurits@gmail.com>
2016-11-02 14:34:43 -0500

    Added blocks to HTML dynamically

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-  
+  $blockContainer = $('#block-container');
+
+  for (var i = 0; i < 4; i++) {
+    $blockContainer.append('<div class="color-block"></div>');
+  }
 });
 
 function randomNumber(min, max) {
