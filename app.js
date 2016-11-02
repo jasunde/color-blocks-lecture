7a51e4328cb630840616d1f60a18e74a21b4a494
jasunde <jlaurits@gmail.com>
2016-11-02 14:59:35 -0500

    Added color block click listener testing success

--- app.js
+++ app.js
@@ -10,17 +10,26 @@ $(document).ready(function () {
 
   for (var i = 0; i < colors.length; i++) {
     var $block = $('<div class="color-block"></div>')
-      .css('background-color', colors[i]);
+      .css('background-color', colors[i])
+      .addClass(colors[i]);
 
     $blockContainer.append($block);
   }
 
   pickColor();
 
+  $blockContainer.on('click', '.color-block', function () {
+    if($(this).hasClass(currentColor)) {
+      console.log('got it!');
+    } else {
+      console.log('nope!')
+    }
+  });
+
   function setPrompt() {
     $('#prompt').text('Please click the ' + currentColor + ' box!');
   }
-  
+
   function pickColor() {
     currentColor = colors[randomNumber(0, colors.length - 1)];
     setPrompt();
